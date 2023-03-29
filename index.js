7fefd99787b604a5fc6b472c9b6d2a29becfce9a
worawut srimala <worawut48@hotmail.co.th>
2023-03-29 15:30:52 +0700

    fix style

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express"); 
 const cors = require("cors"); 
+const sqlQuery = require("./sql_query")
+
+const app = express();
+const port = 5174;
 
 const handleGetSelectBranch = require("./src/controller/select_branch");
 const handleGetThawornRole = require("./src/controller/thaworn_role");
 const handleGetSelectPlaceType = require("./src/controller/select_place_type");
 const handleGetSelectPlace = require("./src/controller/select_place");
 const handleGetMenuByRoleId = require("./src/controller/menu_list");
-const sqlQuery = require("./sql_query")
-
-const app = express();
-const port = 5174;
 
 sqlQuery()
 
