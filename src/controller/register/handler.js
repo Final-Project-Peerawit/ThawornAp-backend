0d2e3a7733251b552b285051407f94cd4ab92b7e
Peerawit Wiboonthanakun <Peerawit2000@gmail.com>
2023-03-29 17:27:45 +0700

    add uuid

--- src/controller/register/handler.js
+++ src/controller/register/handler.js
@@ -1,10 +1,10 @@
 const con = require("../../connection");
 const bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require("uuid");
 
 const handlerRegister = (req, res) => {
   try {
     const {
-      login_id,
       role_id,
       branch_id,
       firstname,
@@ -13,6 +13,9 @@ const handlerRegister = (req, res) => {
       phone_number,
       password,
     } = req.body;
+
+    const login_id = uuidv4();
+
     bcrypt.hash(password, 10, (err, hash) => {
       con.query(
         `INSERT INTO USER_INFO (login_id , role_id, branch_id, firstname, lastname ,email ,phone_number, password) VALUES ('${login_id}','${role_id}','${branch_id}','${firstname}','${lastname}','${email}','${phone_number}','${hash}')`,
@@ -26,7 +29,7 @@ const handlerRegister = (req, res) => {
       );
     });
   } catch (err) {
-    return res.status(500).send();
+    return res.status(500).send(err);
   }
 };
 
